933e58956f365069ae6c50d488ac7b1937e6dd72
Michael van Dalen <michaelvdalen191@gmail.com>
2023-09-03 20:00:32 +0200

    Docstrings added to views.py file in user_auth

--- user_auth/views.py
+++ user_auth/views.py
@@ -7,41 +7,57 @@ from django.contrib.auth.views import LoginView
 
 # Create your views here.
 def user_login(request):
+    """
+    Renders the login page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered login page.
+    """
     return render(request, 'authentication/login.html')
 
 class CustomLoginView(LoginView):
+    """
+    Custom login view that redirects authenticated users to a specific URL.
+    """
     redirect_authenticated_user = True
 
 def authenticate_user(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
+        """
+    Authenticates a user based on POST data.
 
-    if user is None:
-        return HttpResponseRedirect(
-        reverse('user_auth:login')
-        )
-    else:
-        login(request, user)
-        return HttpResponseRedirect(
-        reverse('user_auth:show_user')
-        )
+    Args:
+        request (HttpRequest): The HTTP request object.
 
-def authenticate_user(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is None:
-        return HttpResponseRedirect(
+    Returns:
+        HttpResponseRedirect: Redirects to the appropriate URL.
+    """
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return HttpResponseRedirect(
             reverse('user_auth:login')
-        )
-    else:
-        login(request, user)
-        return HttpResponseRedirect(
+            )
+        else:
+            login(request, user)
+            return HttpResponseRedirect(
             reverse('user_auth:show_user')
-        )
+            )
 
 def show_user(request):
+    """
+    Renders the user's profile page.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered user profile page.
+    """
     print(request.user.username)
     return render(request, 'authentication/user.html', {
         "username": request.user.username,
@@ -49,6 +65,15 @@ def show_user(request):
     })
 
 def register_user(request):
+    """
+    Renders the user registration page and handles form submission.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered registration page or a redirect to the login page.
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
